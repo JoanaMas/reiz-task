c2b9e6e429b0043b51f4e6c86a67927033b8436d
JoanaMas <joana.mastianica@gmail.com>
2024-06-07 07:26:51 +0300

    feat: data list displayed

--- reiz-task-internship/src/App.tsx
+++ reiz-task-internship/src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import styles from './styles/app.module.scss';
 
 
 // Single country interface
@@ -21,11 +22,10 @@ const App = () => {
   useEffect(() => {
     setLoading(true);
 
-
       const fetchCountries = async () => {
 
         try {
-          const result = await axios.get('https://restcountries.com/v2/all?fields=name,region,area');
+          const result = await axios.get<Countries>('https://restcountries.com/v2/all?fields=name,region,area');
           setCountries(result.data);
         } catch (error) {
           console.error('Something went wrong! Failed to fetch data');
@@ -38,12 +38,18 @@ const App = () => {
 
   }, []);
 
-  console.log(countries);
-
   return (
-    <div>
-      {loading && <h4>Please be patient! Data is loading...</h4>}
-      {JSON.stringify(countries)}
+    <div className={styles.container}>
+      <h1>List of countries:</h1>
+      <ol className={styles.listContainer}>
+        {loading && <h4>Data is loading...</h4>}
+        {countries.map((country, index) =>
+          <div key={index} className={styles.card}>
+            <li>{country.name}</li>
+            <li>{country.region}</li>
+            <li>{country.area}</li>
+          </div>)}
+      </ol>
     </div>
   );
 };
